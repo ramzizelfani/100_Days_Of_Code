4c6b40444ffc348b886518c2838ddc9185a13f64
Ramzi Zelfani <ramzi.zelfani@gmail.com>
2020-06-14 00:49:41 +0200

    Adding From submit handling to the edgeledger website

--- HTML_CSS_MasterClass/edgeledger_website/js/main.js
+++ HTML_CSS_MasterClass/edgeledger_website/js/main.js
@@ -20,16 +20,16 @@ window.addEventListener('scroll', () => {
   }
 });
 // Smooth Scrolling with jquery
-$('#navbar a, .btn').on('click', function (e) {
-  if (this.hash !== '') {
-    e.preventDefault();
-    const hash = this.hash;
+// $('#navbar a, .btn').on('click', function (e) {
+//   if (this.hash !== '') {
+//     e.preventDefault();
+//     const hash = this.hash;
 
-    $('html, body').animate(
-      {
-        scrollTop: $(hash).offset().top - 70,
-      },
-      800
-    );
-  }
-});
+//     $('html, body').animate(
+//       {
+//         scrollTop: $(hash).offset().top - 70,
+//       },
+//       800
+//     );
+//   }
+// });
